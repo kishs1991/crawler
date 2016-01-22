7ca0f9498bb233523cd70315041d7a3bc26e104e
kishs1991 <kishs1991@gmail.com>
2016-01-22 12:09:19 +0530

    main added version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 ##################################
 #This is main program for crawler#
 ##################################
-print "crawler version 0.0.0"
+print "************* crawler version 0.0.0 *************"

8c3c8dc1b601d813942cbe1980c99739ff4f51ca
kishs1991 <kishs1991@gmail.com>
2016-01-22 14:01:37 +0530

    main added with basic webpage parsing

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 ##################################
 #This is main program for crawler#
 ##################################
+import urllib
+import HTMLParser
 print "************* crawler version 0.0.0 *************"
+url = "http://www-rohan.sdsu.edu/~gawron/index.html"
+url_handle = urllib.urlopen(url);
+html_page = url_handle.read();
+#print html_page;
+
+urlText = []
+
+class parseText(HTMLParser.HTMLParser):
+	def handle_data(self,data):
+		if data != '\n':
+			urlText.append(data);
+
+# create object of parseText
+lParser = parseText();
+thisurl = "http://www-rohan.sdsu.edu/~gawron/index.html"
+#Feed HTML file into parser
+lParser.feed(urllib.urlopen(thisurl).read())
+lParser.close();
+for item in urlText:
+	print item;
+
